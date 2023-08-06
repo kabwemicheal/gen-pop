d867e67474f044c232b0065304480e8035bee73e
kabwemicheal <kabwemusakanya@gmail.com>
2023-08-06 23:03:05 +0200

    feat: feature rafactor

---
+++ src/feature-components/Home/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopNav from '../feature-nav-bar/TopNav'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <TopNav/>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/feature-components/feature-borrower/CreateBorrower.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BorrowerModal from './BorrowerModal';
+
+const CreateBorrower = () => {
+  return (
+    < >
+       <BorrowerModal />
+    </>
+  )
+}
+
+export default CreateBorrower;
