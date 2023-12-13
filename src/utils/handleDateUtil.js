16143bc5f28d7ebc53b9abdabd288a23bd137430
kabwemicheal <kabwemusakanya@gmail.com>
2023-12-13 12:48:45 +0200

    Feat: changes 13-12-23

---
+++ src/utils/handleDateUtil.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const handleDateUtil = (dateString, monthsToAdd) => {    
+    // Convert the input date string to a Date object
+        let currentDate = new Date(dateString);
+        // Calculate the new month value
+        let newMonth = currentDate.getMonth() + Number(monthsToAdd);
+    //console.log(newMonth)
+    if (newMonth > 11) {
+        let yearIncrement = Math.floor(newMonth / 12);
+        
+        currentDate.setFullYear(currentDate.getFullYear() + yearIncrement);
+        currentDate.setMonth(newMonth % 12);
+      }
+        // Adjust the year and month
+        currentDate.setMonth(newMonth);
+
+        //console.log("date:", currentDate)
+      
+        // Handle cases where the new month value goes beyond December
+        // and adjust the year accordingly
+       
+      
+        // Return the new date
+     return currentDate
+      
+
+}
+
+export default handleDateUtil
